e176c9b0427f3af48ede3f1b95b275422cb3c53b
Dan Marchese <dan@marchese.me>
2025-06-25 23:11:09 -0700

    Setting up FastAPI endpoints

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import boto3
+from fastapi import FastAPI
+from pydantic import BaseModel
 from strands import Agent
 from strands.models import BedrockModel
 
 
-def main():
-    session = boto3.Session(profile_name="cosmo")
-    bedrock_model = BedrockModel(
-        model_id="us.anthropic.claude-3-5-sonnet-20241022-v2:0", boto_session=session
-    )
+class HelloResponse(BaseModel):
+    """Response from the hello endpoint"""
 
+    name: str
+    anagram: str
+
+
+app = FastAPI()
+session = boto3.Session(profile_name="cosmo")
+bedrock_model = BedrockModel(
+    model_id="us.anthropic.claude-3-5-sonnet-20241022-v2:0", boto_session=session
+)
+
+
+@app.get("/")
+def root() -> str:
+    return "🤖 Cosmo Server"
+
+
+@app.get("/hello/{first_name}/{middle_name}/{last_name}")
+def anagram_greeter(first_name: str, middle_name: str, last_name: str) -> HelloResponse:
     agent = Agent(model=bedrock_model)
-    agent("Tell me about Amazon Bedrock.")
-    print()
-
-
-if __name__ == "__main__":
-    main()
+    return agent.structured_output(
+        HelloResponse,
+        f"Come up with a fun anagram for '{first_name} {middle_name} {last_name}'",
+    )
